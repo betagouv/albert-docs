128d3f1edb0fcc1ace4f4e8e4652308c0d11fc76
Julien Bouquillon <julien.bouquillon@beta.gouv.fr>
2024-12-13 14:03:17 +0100

    fix: basePath

--- next.config.mjs
+++ next.config.mjs
@@ -16,7 +16,7 @@ const version = pkg.version;
 
 /** @type {import('next').NextConfig} */
 const moduleExports = {
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  //basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx"],
   reactStrictMode: true,
   output: "standalone",
